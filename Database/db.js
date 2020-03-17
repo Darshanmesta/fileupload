704a3f37e55792edd08a9d1a0580a71d586aa408
Darshanmesta <46439087+Darshanmesta@users.noreply.github.com>
2020-03-17 21:13:19 +0530

    Add files via upload

---
+++ Database/db.js
@@ -0,0 +1,3 @@
+module.exports={
+    DB:"mongodb+srv://DarshanKA47:DarshanKA47@cluster0-kiwag.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+}
